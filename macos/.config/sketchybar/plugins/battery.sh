ff41881c0d1604f1350cfa390215d086cdf2c271
王禹茗 <wangyuming@ft.tech>
2026-03-16 17:13:09 +0800

    battery

--- macos/.config/sketchybar/plugins/battery.sh
+++ macos/.config/sketchybar/plugins/battery.sh
@@ -6,23 +6,28 @@ PERCENTAGE="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
 CHARGING="$(pmset -g batt | grep 'AC Power')"
 
 if [ "$PERCENTAGE" = "" ]; then
-  exit 0
+    sketchybar --set "$NAME" icon="" label="NoBattery"
+    exit 0
 fi
 
 case "${PERCENTAGE}" in
-  9[0-9]|100) ICON=""
-  ;;
-  [6-8][0-9]) ICON=""
-  ;;
-  [3-5][0-9]) ICON=""
-  ;;
-  [1-2][0-9]) ICON=""
-  ;;
-  *) ICON=""
+9[0-9] | 100)
+    ICON=""
+    ;;
+[6-8][0-9])
+    ICON=""
+    ;;
+[3-5][0-9])
+    ICON=""
+    ;;
+[1-2][0-9])
+    ICON=""
+    ;;
+*) ICON="" ;;
 esac
 
 if [[ "$CHARGING" != "" ]]; then
-  ICON=""
+    ICON=""
 fi
 
 sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%"
